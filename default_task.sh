e4786ae9ffe763a94a3208108bf8fb1cb3cddaed
Clackz <997536907@qq.com>
2021-07-10 18:57:48 +0800

    fix

--- default_task.sh
+++ default_task.sh
@@ -222,7 +222,8 @@ if [ -n "$ENABLE_AUTO_HELP" ]; then
   if [ "$ENABLE_AUTO_HELP" = "true" ]; then
     echo "开启自动助力"
     #在所有脚本执行前，先执行助力码导出
-    sed -i 's/node/ . \/scripts\/docker\/auto_help.sh export > \/scripts\/logs\/auto_help_export.log \&\& node /g' ${mergedListFile}
+    sed -i 's/\snode/ . \/scripts\/docker\/auto_help.sh export > \/scripts\/logs\/auto_help_export.log \&\& node /g' ${mergedListFile}
+    sed -i 's/\snpx/ . \/scripts\/docker\/auto_help.sh export > \/scripts\/logs\/auto_help_export.log \&\& npx /g' ${mergedListFile}
   else
     echo "未开启自动助力"
   fi
